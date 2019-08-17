d46476665880dbdc3bf70c8868f2dbe83c5f242b
kumar ankit <ankitech@gmail.com>
2019-08-17 14:35:15 +0530

    add lombok support. #7
    Change-Id: I6968cf7b155a0e7835378c37beab09b914da12e0

--- src/main/java/com/quizam/domain/Question.java
+++ src/main/java/com/quizam/domain/Question.java
@@ -1,13 +1,18 @@
 package com.quizam.domain;
 
+import lombok.*;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 import org.springframework.hateoas.ResourceSupport;
 
 import java.util.List;
-import java.util.Objects;
 
 @Document
+@Getter
+@Setter
+@AllArgsConstructor
+@ToString
+@EqualsAndHashCode(callSuper = true)
 public class Question extends ResourceSupport {
     @Id
     private String questionId;
@@ -17,104 +22,4 @@ public class Question extends ResourceSupport {
     private String author;
     private String subject;
     private String topic;
-
-
-    public Question(String questionId, String question, List<String> options, List<String> answers, String author, String subject, String topic) {
-        this.questionId = questionId;
-        this.question = question;
-        this.options = options;
-        this.answers = answers;
-        this.author = author;
-        this.subject = subject;
-        this.topic = topic;
-    }
-
-    public String getAuthor() {
-        return author;
-    }
-
-    public void setAuthor(String author) {
-        this.author = author;
-    }
-
-    public String getSubject() {
-        return subject;
-    }
-
-    public void setSubject(String subject) {
-        this.subject = subject;
-    }
-
-    public String getTopic() {
-        return topic;
-    }
-
-    public void setTopic(String topic) {
-        this.topic = topic;
-    }
-
-    public String getQuestionId() {
-        return questionId;
-    }
-
-    public void setQuestionId(String questionId) {
-        this.questionId = questionId;
-    }
-
-    public String getQuestion() {
-        return question;
-    }
-
-    public void setQuestion(String question) {
-        this.question = question;
-    }
-
-    public List<String> getOptions() {
-        return options;
-    }
-
-    public void setOptions(List<String> options) {
-        this.options = options;
-    }
-
-    public List<String> getAnswers() {
-        return answers;
-    }
-
-    public void setAnswers(List<String> answers) {
-        this.answers = answers;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (!(o instanceof Question)) return false;
-        if (!super.equals(o)) return false;
-        Question question1 = (Question) o;
-        return getQuestionId().equals(question1.getQuestionId()) &&
-                getQuestion().equals(question1.getQuestion()) &&
-                getOptions().equals(question1.getOptions()) &&
-                getAnswers().equals(question1.getAnswers()) &&
-                getAuthor().equals(question1.getAuthor()) &&
-                getSubject().equals(question1.getSubject()) &&
-                getTopic().equals(question1.getTopic());
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(super.hashCode(), getQuestionId(), getQuestion(), getOptions(), getAnswers(), getAuthor(), getSubject(), getTopic());
-    }
-
-    @Override
-    public String toString() {
-        return "Question{" +
-                "questionId='" + questionId + '\'' +
-                ", question='" + question + '\'' +
-                ", options=" + options +
-                ", answers=" + answers +
-                ", author='" + author + '\'' +
-                ", subject='" + subject + '\'' +
-                ", topic='" + topic + '\'' +
-                '}';
-    }
 }
