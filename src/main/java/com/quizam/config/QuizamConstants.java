028a17b259f94c8629a4f82103301ab2f56cd1c7
shreyaspatil1008 <shreyaspatil1008@gmail.com>
2019-08-26 15:48:15 +0530

    #20 Integration of GraphQL

--- src/main/java/com/quizam/config/QuizamConstants.java
+++ src/main/java/com/quizam/config/QuizamConstants.java
@@ -4,4 +4,5 @@ public class QuizamConstants {
 
     public static final String API_URL = "/quizam";
     public static final String APP_V1 = "/v1";
+    public static final String APP_V2 = "/v2";
 }
